e1cb5d55a6a93313585f6010ac1a9759bab78e40
zaculizate1996 <alancabot.ac@gmail.com>
2020-04-02 04:01:24 -0300

    Se agrega ejercicio area circulo

---
+++ AreaCirculo/src/areacirculo/AreaCirculo.java
@@ -0,0 +1,15 @@
+package areacirculo;
+
+public class AreaCirculo {
+
+    public static void main(String[] args) {
+        
+        int radio = 2;
+        double pi = 3.14;
+        
+        double area = radio * radio * pi;
+        System.out.println("El area del circulo es " + area);
+        
+    }
+    
+}
