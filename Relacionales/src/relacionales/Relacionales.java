a43c6f71990b1b00c8e4dda6525d20698d5167db
zaculizate1996 <alancabot.ac@gmail.com>
2020-04-02 03:55:58 -0300

    Se agrega ejercicio relacionales

---
+++ Relacionales/src/relacionales/Relacionales.java
@@ -0,0 +1,20 @@
+package relacionales;
+
+public class Relacionales {
+    
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        
+        System.out.println("a == b = " + (a == b) );
+        System.out.println("a != b = " + (a != b) );
+        System.out.println("a > b = " + (a > b) );
+        System.out.println("a < b = " + (a < b) );
+        System.out.println("b >= a = " + (b >= a) );
+        System.out.println("b <= a = " + (b <= a) );
+        
+    }
+
+}
+
