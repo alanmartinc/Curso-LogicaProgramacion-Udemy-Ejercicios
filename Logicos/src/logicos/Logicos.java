29648cd796c392351e33a24873f1af3a0fff8e61
zaculizate1996 <alancabot.ac@gmail.com>
2020-04-02 04:00:09 -0300

    Se agrega ejercicio logicos

---
+++ Logicos/src/logicos/Logicos.java
@@ -0,0 +1,16 @@
+package logicos;
+
+public class Logicos {
+
+    public static void main(String[] args) {
+        
+        boolean v = true;
+        boolean f = false;
+        
+        System.out.println("v && f = " + (v&&f) );
+        System.out.println("v || f = " + (v||f) );
+        System.out.println("! (v && f) = " + !(v&&f) );
+        
+    }
+    
+}
